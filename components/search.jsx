843ef2dc1f5a24dff97e5642819e9af786606728
Frank Cairampoma <ingfralch@gmail.com>
2023-06-02 17:41:01 -0500

    frontend search done

---
+++ components/search.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import {
+    View, Text, StyleSheet,
+    TouchableOpacity, SafeAreaView,
+    ScrollView, Image, Platform,
+    ActivityIndicator, ImageBackground,
+    Dimensions, Alert, Linking, StatusBar,
+    TextInput, Button
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function Search() {
+    const [ingrediente, setIngrediente] = useState('');
+    const [ingredientes, setIngredientes] = useState(['huevos', '2 papas', '3 zanahorias', '1 cebolla', '1 tomate', '1/2 taza de leche']);
+
+    const quitarIngrediente = (index) => {
+        let ingredientesTemp = [...ingredientes];
+        ingredientesTemp.splice(index, 1);
+        setIngredientes(ingredientesTemp);
+    }
+
+    return (
+        <View style={styles.container}>
+           
+            <View style={{  marginTop: 50 }}>
+                <View style={{ flexDirection: "row", justifyContent: "center" , marginBottom:50}}>
+                     <Image source={require('../assets/logo/logo_cocina.jpg')} style={{ width: 180, height: 120 }} />
+                </View>
+                <View style={{ flexDirection: "row", justifyContent: "center"}}>
+                    <TextInput 
+                        style={styles.input} placeholder="Ingresar ingredientes: ejem. 2 papas, huevos" 
+                        onChangeText={text => setIngrediente(text)} value={ingrediente}
+                    />
+                    <TouchableOpacity 
+                        style={{ backgroundColor: '#F9CC00', padding: 10, borderRadius: 10, height: 40, justifyContent: "center" }}
+                        onPress={() => { setIngredientes([...ingredientes, ingrediente]); setIngrediente('') }}
+                    >
+                        <AntDesign name="caretright" size={24} color="#383838" />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: "row", justifyContent: "center", marginTop: 10, 
+                flexWrap: "wrap", width: "75%", alignSelf: "center"}}>
+
+                    {ingredientes.map((item, index) => {
+                        return (
+                            <TouchableOpacity 
+                                key={index} 
+                                style={{ backgroundColor: '#F9CC00', padding: 10, borderRadius: 10, height: 40, justifyContent: "center", margin: 5 }}
+                                onPress={() => quitarIngrediente(index)}
+                            >
+                                <Text style={{ color: '#383838' }}>{item}</Text>
+                            </TouchableOpacity>
+                        )
+                    })}
+                </View>
+
+            </View>
+            <View style={[{  }]} >
+                <TouchableOpacity style={{ backgroundColor: '#383838', padding: 10, borderRadius: 10, height: 40, justifyContent: "center", width: "75%", alignSelf: "center" }}>
+                    <Text style={{ color: '#F9CC00', textAlign: "center",   fontWeight: "bold"}}>Buscar recetas</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFEF9',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    input: {
+        height: 40,
+        width: "75%",
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        borderColor: '#bbb',
+    }
+});
